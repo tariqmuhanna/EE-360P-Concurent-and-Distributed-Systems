49860aabe2c7def32292ebb18eaac82e3a542e28
Melanie Feng <melanie.feng@gmail.com>
2019-02-07 13:26:18 -0600

    cyclic barrier

--- HW2/CyclicBarrier.java
+++ HW2/CyclicBarrier.java
@@ -5,14 +5,36 @@
 import java.util.concurrent.Semaphore; // for implementation using Semaphores
 
 public class CyclicBarrier {
-	
+	Semaphore waitParties;
+	Semaphore mutex;
+	int numParties = 0;
+	int set = 0;
+	boolean flag = false;
 	public CyclicBarrier(int parties) {
+		waitParties = new Semaphore(parties, true);
+		mutex = new Semaphore(1, true);
+		numParties = parties;
 	}
 	
 	public int await() throws InterruptedException {
-           int index = 0;
+		mutex.acquire();
+		int place = waitParties.availablePermits() -1;
+		
+		mutex.release();
+		waitParties.acquire();
+		
+		
+		while(waitParties.availablePermits() != 0 || flag == true);
+		
+		//mutex.acquire();
+		flag = false;
+		waitParties.release();
+		if(waitParties.availablePermits() == 0)
+			flag = true;
+		//mutex.release();
 		
           // you need to write this code
-	    return index;
+        
+	    return place;
 	}
 }

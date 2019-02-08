5c3fd09a5e0e39eec463ee0af1b9705a3191b9fc
tariqmuhanna <32185606+tariqmuhanna@users.noreply.github.com>
2019-02-07 21:17:17 -0600

    Completed MCB, requires testing still

--- HW2/MonitorCyclicBarrier.java
+++ HW2/MonitorCyclicBarrier.java
@@ -4,14 +4,40 @@
  */
 
 public class MonitorCyclicBarrier {
-	
+	int initialParties; //total parties
+	int partiesAwait; //parties yet to arrive
+
 	public MonitorCyclicBarrier(int parties) {
+		initialParties=parties;
+		partiesAwait=parties;
 	}
-	
-	public int await() throws InterruptedException {
-           int index = 0;
-		
-          // you need to write this code
-	    return index;
+
+	/**
+	 *  If the current thread is not the last to arrive(i.e. call await() method) then
+	 it waits until one of the following things happens -
+	 - The last thread to call arrive(i,.e. call await() method), or
+	 - Some other thread interrupts the current thread, or
+	 - Some other thread interrupts one of the other waiting threads, or
+	 - Some other thread times out while waiting for barrier, or
+	 - Some other thread invokes reset() method on this cyclicBarrier.
+	 */
+	public synchronized int await() throws InterruptedException {
+
+		partiesAwait--; //decrements awaiting parties by 1.
+
+		//If the current thread is not the last to arrive, thread will wait.
+		if(partiesAwait>0){
+			this.wait();
+		}
+           /*If the current thread is last to arrive, notify all waiting threads, and
+            launch event*/
+		else{
+                  /* All parties have arrive, make partiesAwait equal to initialParties,
+                    so that CyclicBarrier could become cyclic. */
+			partiesAwait = initialParties;
+			notifyAll(); //notify all waiting threads
+		}
+		return partiesAwait;
 	}
 }
+

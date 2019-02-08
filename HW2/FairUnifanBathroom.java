8300892a5e080886c5a51513442a7585ea3b9b36
tariqmuhanna <32185606+tariqmuhanna@users.noreply.github.com>
2019-02-08 15:35:37 -0600

    First attempt on code

--- HW2/FairUnifanBathroom.java
+++ HW2/FairUnifanBathroom.java
@@ -1,22 +1,59 @@
 // EID 1
 // EID 2
 
-public class FairUnifanBathroom {   
-	
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class FairUnifanBathroom {
+    int size = 0;
+    int bathroom_limit = 5;
+    int flag = 0; // 0 = UT, 1 = OU
+//	ReentrantLock monitorLock = new ReentrantLock();
+//	Condition ut = monitorLock.newCondition();
+//    Condition ou = monitorLock.newCondition();
+
   public synchronized void enterBathroomUT() {
-    // Called when a UT fan wants to enter bathroom	
+    // Called when a UT fan wants to enter bathroom
+      if (size != 0) {
+          while (flag == 1 || size == bathroom_limit) {
+              try {
+                  wait();
+              } catch (InterruptedException e) {
+                  e.printStackTrace();
+              }
+          }
+      }
+      else
+          flag = 0;
+      size++;
   }
 	
 	public synchronized void enterBathroomOU() {
     // Called when a OU fan wants to enter bathroom
-	}
+        if (size != 0) {
+            while (flag == 0 || size == bathroom_limit) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        else
+            flag = 1;
+        size++;
+  }
 	
 	public synchronized void leaveBathroomUT() {
     // Called when a UT fan wants to leave bathroom
+        size--;
+        notifyAll();
 	}
 
 	public synchronized void leaveBathroomOU() {
     // Called when a OU fan wants to leave bathroom
+        size--;
+        notifyAll();
 	}
 }
 	

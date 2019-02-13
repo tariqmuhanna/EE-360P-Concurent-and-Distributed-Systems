28661e609cfcb7ffb61426c0116e04c8116fa52b
Melanie Feng <melanie.feng@gmail.com>
2019-02-13 16:50:28 -0600

    yay

--- HW2/CyclicBarrier.java
+++ HW2/CyclicBarrier.java
@@ -1,54 +1,70 @@
 /*
  * EID's of group members
- * 
+ *
  */
 import java.util.concurrent.Semaphore; // for implementation using Semaphores
-import java.util.concurrent.atomic.AtomicInteger;
+
+// Conceptually, a semaphore maintains a set of permits.
+// Each acquire() blocks if necessary until a permit is available,
+// and then takes it.
+
+// Each release() adds a permit, potentially releasing a blocking acquirer
 
 public class CyclicBarrier {
-	Semaphore holdTillFull;
-	Semaphore waitParties;
-	boolean flag = false;
-	int numParties = 0;
-	AtomicInteger count = new AtomicInteger(0);
-	
-	public CyclicBarrier(int parties) {
-		holdTillFull = new Semaphore(parties, true);
-		//wait until all permits from waitParties are acquired before trying to obtain a holdTillFull permit,
-		waitParties = new Semaphore(parties, true);
-		numParties = parties;
-		
-	}
-	
-	public int await() throws InterruptedException {
-		
-	
-		waitParties.acquire();	//try to get a permit
-		
-		//System.out.println("Thread" + Thread.currentThread().getId() + " has gotten waitParties permit ");
-		
-		while(flag || count.get() >= numParties) {
-			//wait here until 5 threads have gotten permits
-		}
-		int place = count.getAndIncrement();
-		holdTillFull.acquire();
-		
-		//System.out.println("Thread" + Thread.currentThread().getId() + " has obtained holdTillFull at " + place);
-		
-		
-		while(count.get() != numParties && !flag) {}
-		//System.out.println(place+ " Thread" + Thread.currentThread().getId() + " has count--");
-		
-		flag = true;
-    	//System.out.println(place + " flag to true");
-		//System.out.println(place + " Thread" + Thread.currentThread().getId() + " is releasing permits");
-		waitParties.release();
-		holdTillFull.release();
-		count.getAndDecrement();
-        if(holdTillFull.availablePermits() == numParties) {
-        	flag = false;
-        	//System.out.println(place +" flag to false");
+    int parties;
+    int count;
+    Semaphore mutex;
+    Semaphore barrier1, barrier2;
+
+    public CyclicBarrier(int parties) {
+        // Creates a new CyclicBarrier that will release threads only when
+        // the given number of threads are waiting upon it
+        this.parties = parties;              // number of threads
+        this.count = 0;	                 // start with zero threads in the beginning
+        mutex = new Semaphore(1);	 // mutual exclusion semaphore
+        barrier1 = new Semaphore(0);
+        barrier2 = new Semaphore(1);
+    }
+
+    public int await() throws InterruptedException {
+        // Waits until all parties have invoked await on this CyclicBarrier.
+        // If the current thread is not the last to arrive then it is
+        // disabled for thread scheduling purposes and lies dormant until
+        // the last thread arrives.
+        // Returns: the arrival index of the current thread, where index
+        // (parties - 1) indicates the first to arrive and zero indicates
+        // the last to arrive.
+        int index = 0;
+
+        mutex.acquire();        // holds lock
+        index = count;          // index = count
+        count++;
+
+        if(count == parties) {  // if # of parties = count, begin releasing
+            barrier2.acquire();
+            barrier1.release();
         }
-	    return place;
-	}
+
+        mutex.release();        // release lock
+
+        barrier1.acquire();     // will block until another thread calls release()
+        barrier1.release();     // n threads will wait while threads release on another
+
+
+
+        mutex.acquire();
+        count--; //All threads decrement, resetting by one
+
+        //Nth thread closes barrier1 and opens barrier 2(resets Cyclic Barrier)
+        if(count == 0){
+            barrier1.acquire(); //Close barrier 1
+            barrier2.release(); //Let one thread go through barrier
+        }
+        mutex.release();
+
+        barrier2.acquire(); //All the N thread will wait here after resetting
+        barrier2.release(); //Each thread passes through will signal one other thread
+
+        return index;
+    }
 }

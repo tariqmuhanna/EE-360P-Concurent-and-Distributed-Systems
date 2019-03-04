11502fb936f01be8ac5538376364713eb78d7b72
Melanie Feng <melanie.feng@gmail.com>
2019-03-03 22:13:39 -0600

    made udp a thread

---
+++ HW3/inventory.java
@@ -0,0 +1,10 @@
+public class inventory{
+	String name;
+	String color;
+	int q;
+	public inventory(String name, String color, int quant) {
+		this.name = name;
+		this.color = color;
+		q = quant;
+	}
+}
